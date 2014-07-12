c46625bbe42b635d2f9228856a4a516b19f39764
airyland <airyland@qq.com>
2014-07-12 20:05:15 +0800

    add substitute function

---
+++ README.md
@@ -0,0 +1,27 @@
+# util
+
+---
+
+[![spm version](http://spmjs.io/badge/util)](http://spmjs.io/package/util)
+
+An awesome spm package!
+
+---
+
+## Install
+
+```
+$ spm install util --save
+```
+
+## Usage
+
+```js
+var util = require('util');
+// use util
+```
+
+## Api
+
+Here is more details.
+
---
+++ index.js
@@ -0,0 +1,21 @@
+exports.substitute = function(string, object, regexp) {
+    return String(string).replace(regexp || (/\\?\{([^{}]+)\}/g), function(match, name) {
+        if (match.charAt(0) == '\\') return match.slice(1);
+        if (object[name] != null) return object[name];
+
+        var path = name.split('.'),
+            length = path.length,
+            sub = object,
+            i;
+
+        if (length <= 1)
+            return match;
+
+        for (i = 0; i < length; i++) {
+            if ((sub = sub[path[i]]) == null) return match;
+        }
+        return sub;
+    });
+}
+
+
